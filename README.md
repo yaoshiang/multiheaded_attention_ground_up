ea21ce984c09be5e82d0dcdddcf45daa39b85091
Yaoshiang Ho Masterful <yaoshiang@masterfulai.com>
2023-04-05 13:29:18 -0700

    Included log output and renamed axis.

--- README.md
+++ README.md
@@ -8,6 +8,74 @@ The Image Tokenizer is implemented as both a looping and a vectorized approach u
 
 No Keras layers for attention/transformers were used. 
 
+Output from a few epochs:
+
+```
+/home/yaoshiang/miniconda3/envs/tf/lib/python3.8/site-packages/tensorflow_addons/utils/ensure_tf_install.py:53: UserWarning: Tensorflow Addons supports using Python ops for all Tensorflow versions above or equal to 2.7.0 and strictly below 2.10.0 (nightly versions are not supported). 
+ The versions of TensorFlow you are currently using is 2.6.2 and is not supported. 
+Some things might work, some things might not.
+If you were to encounter a bug, do not file an issue.
+If you want to make sure you're using a tested and supported configuration, either change the TensorFlow version or the TensorFlow Addons's version. 
+You can find the compatibility matrix in TensorFlow Addon's readme:
+https://github.com/tensorflow/addons
+  warnings.warn(
+Model: "model"
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #   
+=================================================================
+input_1 (InputLayer)         [(None, 224, 224, 3)]     0         
+_________________________________________________________________
+image_tokenizer (ImageTokeni (None, 196, 64)           49344     
+_________________________________________________________________
+multi_headed_attention (Mult (None, 4, 196, 64)        83200     
+_________________________________________________________________
+multi_headed_attention_1 (Mu (None, 4, 196, 64)        83200     
+_________________________________________________________________
+multi_headed_attention_2 (Mu (None, 4, 196, 64)        83200     
+_________________________________________________________________
+multi_headed_attention_3 (Mu (None, 4, 196, 64)        83200     
+_________________________________________________________________
+tf.math.reduce_mean (TFOpLam (None, 4, 64)             0         
+_________________________________________________________________
+flatten (Flatten)            (None, 256)               0         
+_________________________________________________________________
+dense (Dense)                (None, 10)                2570      
+=================================================================
+Total params: 384,714
+Trainable params: 384,714
+Non-trainable params: 0
+_________________________________________________________________
+Epoch 1/100
+252/252 [==============================] - 10s 22ms/step - loss: 1.9323 - acc: 0.3215 - val_loss: 1.6581 - val_acc: 0.4549
+Epoch 2/100
+252/252 [==============================] - 5s 21ms/step - loss: 1.4949 - acc: 0.5027 - val_loss: 1.3980 - val_acc: 0.5139
+Epoch 3/100
+252/252 [==============================] - 5s 21ms/step - loss: 1.3254 - acc: 0.5552 - val_loss: 1.2596 - val_acc: 0.5729
+Epoch 4/100
+252/252 [==============================] - 5s 21ms/step - loss: 1.2180 - acc: 0.5926 - val_loss: 1.2504 - val_acc: 0.5833
+Epoch 5/100
+252/252 [==============================] - 5s 21ms/step - loss: 1.1359 - acc: 0.6219 - val_loss: 1.0463 - val_acc: 0.6562
+Epoch 6/100
+252/252 [==============================] - 5s 21ms/step - loss: 1.0690 - acc: 0.6494 - val_loss: 1.0522 - val_acc: 0.6458
+Epoch 7/100
+252/252 [==============================] - 5s 21ms/step - loss: 1.0205 - acc: 0.6631 - val_loss: 1.0125 - val_acc: 0.6736
+Epoch 8/100
+252/252 [==============================] - 5s 21ms/step - loss: 0.9842 - acc: 0.6736 - val_loss: 1.0019 - val_acc: 0.6806
+Epoch 9/100
+252/252 [==============================] - 5s 21ms/step - loss: 0.9487 - acc: 0.6841 - val_loss: 0.9852 - val_acc: 0.6701
+Epoch 10/100
+252/252 [==============================] - 5s 21ms/step - loss: 0.9174 - acc: 0.6998 - val_loss: 1.1007 - val_acc: 0.6458
+Epoch 11/100
+252/252 [==============================] - 5s 21ms/step - loss: 0.8810 - acc: 0.7077 - val_loss: 0.9471 - val_acc: 0.7049
+Epoch 12/100
+252/252 [==============================] - 5s 21ms/step - loss: 0.8565 - acc: 0.7180 - val_loss: 0.9442 - val_acc: 0.6771
+Epoch 13/100
+252/252 [==============================] - 5s 21ms/step - loss: 0.8248 - acc: 0.7309 - val_loss: 0.8553 - val_acc: 0.7431
+Epoch 14/100
+252/252 [==============================] - 5s 21ms/step - loss: 0.8096 - acc: 0.7369 - val_loss: 0.8737 - val_acc: 0.7326
+```
+
+
 Copyright 2023 Yaoshiang Ho
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
--- mha.py
+++ mha.py
@@ -48,13 +48,14 @@ def get_ds(batch_size, train_per, valid_per, shape=(224, 224, 3)):
       lambda img, label: (tf.image.convert_image_dtype(img, tf.float32), label),
       num_parallel_calls=AUTOTUNE,
   )
-  # Alexnet:
-  # Preprocessing: resize 1:1 so that the short edge is 256, then center crop to 256x256.
-  # Augmentation: Take random 224x224 crops for train; for valid/test, take the four corners + center at 224x224.
 
   assert shape[0] == 224
 
   def _alexnet_resize_one_hot(img, label):
+    """Alexnet preprocessing.
+    
+    Resize 1:1 so that the short edge is 256, then center crop to 256x256.
+    """
     orig_hwc = tf.shape(img)
     if orig_hwc[0] > orig_hwc[1]:
       img = tf.transpose(img, [1, 0, 2])
@@ -88,11 +89,16 @@ def get_ds(batch_size, train_per, valid_per, shape=(224, 224, 3)):
   dataset_train = dataset_train.shuffle(1000)
 
   def _alexnet_crop_train(img):
+    """Alexnet augmentation for train.
+     
+    Take random 224x224 crops.
+    """
     y = RANDGEN.uniform(shape=(), minval=0, maxval=32, dtype=tf.int32)
     x = RANDGEN.uniform(shape=(), minval=0, maxval=32, dtype=tf.int32)
     return img[y:y + 224, x:x + 224, :]
 
   # def _alexnet_crop_valid(ds: tf.data.Dataset) -> tf.data.Dataset:
+
   #   ds = ds.map(lambda img, label: (
   #       tf.stack((
   #           (img[0:224, 0:224, :]),  # NW
@@ -150,18 +156,16 @@ class ImageTokenizer(tf.keras.layers.Layer):
 
   @staticmethod
   def chip_image(image, B, H, W, P):
-    """Chops an image into chips.
+    """Turns an image into a sequence of embeddings. 
 
-    TODO: Test a looping implementation. Although this algo is vectorized,
-    it relies on the very expensive transpose operation. It may be
-    faster to loop in code, and allow TF to unroll the loop.
+    Includes both positional and image embeddings.
     
     Args:
       image: A tensor of shape [batch_size, height, width, 3].
-      batch_size: An integer.
-      height: An integer.
-      width: An integer.
-      patch_size: An integer.
+      B: Batch size.
+      H: Height of image.
+      W: Width of image. 
+      P: Patch size, e.g. 32x32.
 
     Returns:
       A tensor of shape [batch_size, height // patch_size, width // patch_size,
@@ -169,9 +173,9 @@ class ImageTokenizer(tf.keras.layers.Layer):
     """
     tf.debugging.assert_equal(H, W)
 
-    looping = True
+    vectorized = True
 
-    if looping:
+    if vectorized == False:
       image = tf.reshape(image, (-1, H, W * 3))
 
       chips = []
@@ -204,6 +208,7 @@ class ImageTokenizer(tf.keras.layers.Layer):
 
       return image
 
+  ### This is where the embeddings are created from the chips.
   def call(self, image):
     chips = self.chip_image(image, self.B, self.H, self.W, self.P)
 
@@ -222,6 +227,10 @@ class ImageTokenizer(tf.keras.layers.Layer):
 
 
 class MultiHeadedAttention(tf.keras.layers.Layer):
+  """Multi-headed attention layer.
+
+  Implemented from the ground up using tf.einsum. 
+  """
 
   def __init__(self, embedding_length, num_heads):
     super().__init__()
@@ -236,10 +245,11 @@ class MultiHeadedAttention(tf.keras.layers.Layer):
 
   def build(self, input_shape):
     if len(input_shape) == 3:
-      # BTE
+      # This is the first layer, so we need to create a new axis
+      # for the Heads dimension.
       self.token_input = True
     elif len(input_shape) == 4:
-      # BHTE
+      # BHTD
       self.token_input = False
     else:
       assert False
@@ -256,19 +266,34 @@ class MultiHeadedAttention(tf.keras.layers.Layer):
         shape=(1, H, E),
         initializer=tf.keras.initializers.glorot_normal(),
         trainable=True)
+
     self.query_kernel = self.add_weight(
         name='query_kernel',
         shape=(1, H, E),
         initializer=tf.keras.initializers.glorot_normal(),
         trainable=True)
+
     self.value_kernel = self.add_weight(
         name='value_kernel',
         shape=(1, H, E, E),
         initializer=tf.keras.initializers.glorot_normal(),
         trainable=True)
+
+    self.value_bias = self.add_weight(
+        name='value_bias',
+        shape=(1, H, 1, E),
+        initializer=tf.keras.initializers.glorot_normal(),
+        trainable=True)
+
     self.ff_kernel = self.add_weight(
         name='ff_kernel',
         shape=(1, H, H, E, E),
+        initializer=tf.keras.initializers.glorot_normal(),
+        trainable=True)
+
+    self.ff_bias = self.add_weight(
+        name='ff_bias',
+        shape=(1, H, 1, E),
         initializer=tf.keras.initializers.glorot_normal(),
         trainable=True)
 
@@ -278,55 +303,101 @@ class MultiHeadedAttention(tf.keras.layers.Layer):
       key_kernel,
       query_kernel,
       value_kernel,
+      value_bias,
       layer_norm1,
       ff_kernel,
+      ff_bias,
       layer_norm2,
       activation,
   ):
     """Run multiheaded attention matrix math.
-    
+
+    Takes a batch of embeddings and runs multiheaded attention on them.
+
+    First, keys, queries, and values are computed from the embedding. 
+    Then, the attention matrix is computed as a outer product of K and Q. 
+    The attention matrix is what mixes information between tokens.
+
+    The attention matrix is then softmaxed so that it acts as a 
+    gate or index into the newly generated Values. 
+
+    that value, now called Z, is added to the embedding, and then
+    passed through a feed forward network that mixes the information
+    at each token location between heads, aking to a 1x1 pointwise
+    convolution. Finally, bias, activation, and normalization are applied.    
+
     B = batch size.
-    T = number of tokens.
-    E = embedding length. 
+    H = number of heads. 
+    T = number of tokens / embeddings.
+    D = dimension of the token / embeddings, aka length of the vector. 
 
     Params:
-      token: Tensor of shape BHTE (or BTE if first layer).
-      K: Kernel of shape 1HE
-      Q: Kernel of shape 1HE
-      V: Kernel of shape 1HEE
-      FF: Kernel of shape 1HHE
+      embedding: BTD or BHTD
+      key_kernel: 1HD
+      query_kernel: 1HD
+      value_kernel: 1HdD
+      value_bias: 1H1D
+      layer_norm1: 
+      ff_kernel: 1HHdD
+      ff_bias: 1H1D
+      layer_norm2: 
+      activation: activation to run at the end of the feed forward.
+
+    Returns:
+      New embeddings of shape BHTD.
     """
-    # BHTE, BHE -> BHT
-    key = tf.einsum('...TE, ...E -> ...T', embedding, key_kernel)
+    # BHTD, BHE -> BHT
+    key = tf.einsum('...TD, ...D -> ...T', embedding, key_kernel)
 
-    # BHTE, BHE -> BHT
-    query = tf.einsum('...TE, ...E -> ...T', embedding, query_kernel)
+    # BHTD, BHE -> BHT
+    query = tf.einsum('...TD, ...D -> ...T', embedding, query_kernel)
 
-    # BHTE, BHeE -> BHTE
-    # This is akin to a depthwise conv2D.
-    value = tf.einsum('...TE, ...Ee -> ...Te', embedding, value_kernel)
+    # BHTD, BHeE -> BHTD
+    value = tf.einsum('...TD, ...Dd -> ...TD', embedding, value_kernel)
+    value = value + value_bias
 
     # Cross product of the keys and queries to generate indices on each value.
-    # BHT, BHt -> BHTt
-    # This is the global information mixing that is unlike any
-    # existing conv structure. Maxpool and atrous convs
-    # are still local in nature, not global.
+    #
+    # This is the heart ofthe information mixing that is unlike any
+    # existing conv structure. Information from anywhere in the image,
+    # within the same head, is mixed in. Note that the depth of
+    # the embedding (typically 512) is somewhat akin to the number of
+    # channels in a convolution. By comparison, maxpool and atrous convs are
+    # only able to mix information across an image after multiple layers.
     # The cost of globality is this quadratic expansion, the Tt.
+    #
+    # BHT, BHt -> BHTt
     kq = tf.einsum('...T, ...t -> ...Tt', query, key)
+
+    # Label this as index, because it is a gate or index into the Values.
+    # A softmax (and sigmoid) acting as a gate is a common design
+    # pattern in deep learning, such as the gates in an LSTM.
+    #
+    # BHTt, where summing along t now yields 1.0 as a probability distribution.
     index = tf.nn.softmax(kq, axis=3)
 
     tf.debugging.assert_near(tf.reduce_sum(index[0, 0, 0]), 1.0)
 
-    # Mix the global information, indexed with KxQ.
-    # BHTt, BHtE -> BHTE
-    z = tf.einsum('...Tt, ...tE -> ...TE', index, value)
+    # Apply the index (or probability distribution) by multiplying
+    # the softmaxed values t and summing.
+    # BHTt, BHtD -> BHTD
+    z = tf.einsum('...Tt, ...tD -> ...TD', index, value)
+
+    # Create a residual skip connections and then norm.
+    #
+    # BHTD, BHTD -> BHTD
     z = embedding + z
     z = layer_norm1(z)
 
-    # Mix the information along the head axis.
+    # Mix the information along the head and embedding axis, holding
+    # the token axis fixed.
     # This is akin to a 1x1 point conv2D.
-    # BHTE, BTtEe -> BHte
-    y = tf.einsum('...HTE, ...HhEe -> ...hTe', z, ff_kernel)
+    #
+    # BHTD, BHhEe -> BhTd
+    y = tf.einsum('...HTD, ...HhDd -> ...hTd', z, ff_kernel)
+    y = y + ff_bias
+
+    # Create a residual skip connection, activate, and then norm.
     y = z + y
     y = layer_norm2(activation(y))
 
@@ -334,28 +405,37 @@ class MultiHeadedAttention(tf.keras.layers.Layer):
 
   def call(self, embedding):
     if self.token_input:
-      # Convert BTE to BHTE, where H is 1 but will be broadcast.
+      # Convert BTD to BHTD, where H is 1 but will be broadcast.
       embedding = embedding[:, tf.newaxis, :, :]
 
     # Token is shape BHTL.
     z = self._call(embedding, self.key_kernel, self.query_kernel,
-                   self.value_kernel, self.layer_norm1, self.ff_kernel,
-                   self.layer_norm2, self.activation)
+                   self.value_kernel, self.value_bias, self.layer_norm1,
+                   self.ff_kernel, self.ff_bias, self.layer_norm2,
+                   self.activation)
 
     return z
 
 
-def get_model(model_name, embedding_size=128) -> tf.keras.models.Model:
+def get_model(model_name, embedding_size=64) -> tf.keras.models.Model:
   x = input = tf.keras.Input(shape=(224, 224, 3))
-  x = ImageTokenizer(8, embedding_size)(x)
-  for layer in range(6):
-    x = MultiHeadedAttention(embedding_size, 6)(x)
+  x = ImageTokenizer(16, embedding_size)(x)
+  for layer in range(4):
+    x = MultiHeadedAttention(embedding_size, 4)(x)
 
-  # In a convnet, the penultimate layer is usually a GlobalAveragePooling.
-  # The idea is that the position information is no longer important.
-  # Similarly, we assume that the relative token location is no longer
-  # important.
-  x = tf.reduce_mean(x, axis=2)
+  # In early convnets, the penultimate layer was is usually a Flatten,
+  # making the final layer a linear projection.
+  # A more modern approach is a GlobalAveragePooling layer.
+  # The idea is that the position information is no longer important,
+  # it's the existence of features that matters.
+  #
+  # In our case, an embedding is 128 or 256 features. Should we
+  # mean across heads, tokens, or embedding dimension? The embedding
+  # is analogous to filters so we definitely don't want to lose that.
+  # The Heads is a reasonable guess but it's very small. Let's try
+  # reducing along the tokens dimension for now, and experiment
+  # with other reductions later.
+  x = tf.reduce_mean(x, axis=2)  # BHTD -> BHE
   x = tf.keras.layers.Flatten()(x)
   y = tf.keras.layers.Dense(10)(x)
 
@@ -363,7 +443,7 @@ def get_model(model_name, embedding_size=128) -> tf.keras.models.Model:
   model.build(input_shape=(None, 224, 224, 3))
   model.summary()
 
-  o = tfa.optimizers.LAMB(weight_decay=1e-7)
+  o = tfa.optimizers.LAMB(weight_decay=1e-5)
 
   model.compile(
       optimizer=o,
@@ -380,7 +460,7 @@ def main():
                             shape=(224, 224, 3))
   model = get_model("test_model")
 
-  rlr = tf.keras.callbacks.ReduceLROnPlateau()
+  rlr = tf.keras.callbacks.ReduceLROnPlateau(verbose=1)
 
   model.fit(train, epochs=100, validation_data=val, callbacks=[rlr])
 
